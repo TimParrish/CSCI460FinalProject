5999d21e0c3d5b77b20053834daa4b301fc1e0f7
Tim Parrish <tparrish@wolfssl.com>
2019-11-18 18:30:50 -0700

    initial implementation

--- preemptive_priority_based.c
+++ preemptive_priority_based.c
@@ -1,8 +1,25 @@
 //Pre-emptive priority based
 
-int preemptive_priority_based(void)
-{
-    printf("Printed from: Pre-emptive priority based\n");
+// algorithm for a preemptive priority based scheduler
+int preemptive_priority_based(struct process processes[numProcesses]) {
+	
+	int lowestPriorityProcessIndex = -1;
+	int lowestPriority = k; // first arrival time cannot be after max arrival time
+	
+	// find active process that arrived the earliest
+	for (int i = 0; i < numProcesses; i++) {
+		
+		// check if process is active
+		if (processes[i].active == 1) {
+		
+			// check if the processe's arrival time was earlier than the earliest known
+			if (processes[i].arrivalTime < lowestPriority) {
+				
+				lowestPriority = processes[i].arrivalTime; // set new earleist known arrival time
+				lowestPriorityProcessIndex = i; // return index of the earliest known arrival time		
+			}		
+		}
+	}
 
-    return 0;
+	return lowestPriorityProcessIndex;	
 }
