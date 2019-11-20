a2e558d9743e263acae416afb798c6bbb9c5e9d8
Rusty Clayton <36992543+Rusty89@users.noreply.github.com>
2019-11-20 10:00:26 -0700

    Added Preemptive random

--- preemptive_random.c
+++ preemptive_random.c
@@ -1,1 +1,16 @@
+// algorithm for a random time preemptive random scheduling
+int preemptive_random(struct process processes[]) {
 
+	int selectedProcess = -1;
+  int randomSelect = rand()%numProcesses;
+  if(processes[randomSelect].active == 1){
+    selectedProcess = randomSelect; 
+  }
+
+	// if no valid process was found to return
+	if (selectedProcess = -1;) {
+		return -1;
+	} else {
+		return selectedProcess;
+	}
+}
