38d09481a04735a40ccde05a6fe4263035bd1481
Rusty Clayton <36992543+Rusty89@users.noreply.github.com>
2019-11-20 09:37:28 -0700

    Fixed infinite loop

--- lowest_response_ratio_next.c
+++ lowest_response_ratio_next.c
@@ -1,10 +1,10 @@
 
 //Lowest Response Ratio Next
-
+#define BIG_NUM = 100000
 int lowest_response_ratio_next(struct process processes[numProcesses], int clock, int indexOfRunning, int lrrRunning)
 {
         int indexOfLRRN = -1;
-        int lowestResponseRatio = 0;
+        int lowestResponseRatio = BIG_NUM;
 
         if(lrrRunning == -1) {
                 for(int i = 0; i < numProcesses; i++){
